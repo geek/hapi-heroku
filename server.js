a8be50663b3e366d4092b5d158a250b0d09ca4c8
Wyatt Preul <wpreul@gmail.com>
2013-01-23 10:26:08 -0600

    Adding sample app

---
+++ server.js
@@ -0,0 +1,16 @@
+var Hapi = require('hapi');
+
+var port = process.env.PORT || 5000;
+var server = new Hapi.Server(port);
+
+server.addRoute({ method: 'GET', path: '/{param?}', handler: welcome });
+
+function welcome (request) {
+
+    request.reply('Welcome');
+}
+
+server.start(function () {
+
+    console.log('Server started at ' + server.settings.uri);
+});
