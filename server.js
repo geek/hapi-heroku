61c4397d99df125b891e4853e041d08417253ec8
Wyatt Preul <wpreul@gmail.com>
2013-01-23 10:32:45 -0600

    Parsing port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var Hapi = require('hapi');
 
 var port = process.env.PORT || 5000;
-var server = new Hapi.Server(port);
+var server = new Hapi.Server(parseInt(port));
 
 server.addRoute({ method: 'GET', path: '/{param?}', handler: welcome });
 
